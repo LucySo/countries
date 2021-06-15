70cdfb439fdd58ef755b486c871d78b4831167a2
LucieSaulnier <lucie.saulnier1988@gmail.com>
2021-06-15 13:22:33 +0200

    commit5

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './component/Card'
 
 class App extends React.Component {
 
-  constructor() {
+  constructor () {
     super()
     this.state = {
       name: '',
@@ -15,23 +15,13 @@ class App extends React.Component {
     }
   }
 
-  async componentDidMount () {
-    try {
-      const result = await fetch('https://restcountries.eu/rest/v2/name/france')
-      const country = await result.json()
-      this.setState({
-        
-      })
-    } catch (error) {
-      console.error(error)
-    }
+  componentDidMount () {
+    this.getCountry('brazil')
   }
 
-  async getCountry(country) {
-
+  getCountry = async (country) => {
     try {
-      const result = await fetch('https://restcountries.eu/rest/v2/name/' + country)
-      // const result = await fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+      const result = await fetch(`https://restcountries.eu/rest/v2/name/${country}?fullText=true`)
       const countries = await result.json()
       this.setState({
         name: countries[0].name,
@@ -47,17 +37,23 @@ class App extends React.Component {
 
   render () {
     return <div>
-      <Button onClick={() => this.getCountry('france')}>France</Button>
-      <Button onClick={() => this.getCountry('brazil')}>Brazil</Button>
-      <Button onClick={() => this.getCountry('croatia')}>Croatia</Button>
-      <Card name = {this.state.name} 
-      capital ={this.state.capital} flag = {this.state.flag} 
-      population = {this.state.population}
-      region = {this.state.regio} >
-      </Card>
+      <div className="text-center">
+        <Button onClick={() => this.getCountry('france')}>France</Button>
+        <Button onClick={() => this.getCountry('brazil')}>Brazil</Button>
+        <Button onClick={() => this.getCountry('croatia')}>Croatia</Button>
+        <Button onClick={() => this.getCountry('india')}>India</Button>
+        <Button onClick={() => this.getCountry('taiwan')}>Taiwan</Button>
+        <Button onClick={() => this.getCountry('algeria')}>Algerie</Button>
+        <Button onClick={() => this.getCountry('morocco')}>Maroc</Button>
+        <Button onClick={() => this.getCountry('poland')}>Pologne</Button>
+        <Button onClick={() => this.getCountry('sri lanka')}>Sri Lanka</Button>
+        <Button onClick={() => this.getCountry('congo')}>Congo</Button>
+        <Button onClick={() => this.getCountry('gabon')}>Gabon</Button>
+        <Button onClick={() => this.getCountry('Cambodia')}>Cambodge</Button>
+      </div>
+      <Card {...this.state} /> {/* équivalent à <Card name={this.state.name} ... />*/}
     </div>
   }
 }
 
 export default App
-  
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 
 class Card extends Component {
-    render() {
-        return (
-<div className="card" style={{width: '18rem'}}>
-  <img className="card-img-top" src={this.props.flag} alt="Card image cap"></img>
-  <div className="card-body">
-  <br/>Name : {this.props.name}
-      <br/>Capital : {this.props.capital}
-      <br/>Flag : {this.props.flag}
-      <br/>Population : {this.props.population}
-      <br/>Region : {this.props.region}
-  </div>
-</div>
-        
+  render () {
+    return (
+      <div className="card" style={{ width: '18rem', margin: 'auto' }}>
+        <img className="card-img-top" src={this.props.flag} alt="Flag"/>
+        <h2>Country : {this.props.name}</h2>
+        <h4>Capital : {this.props.capital}</h4>
+        <div>
+          <i className="bi bi-globe2"/>
+          Region : {this.props.region}
+        </div>
+        <div>
+          <i className="bi-people-fill"/>
+          Population : {this.props.population}
+        </div>
+      </div>
     )
-    }
+  }
 }
 
 export default Card
