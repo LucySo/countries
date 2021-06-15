5155845f843bcad9a6435fa650e252ce93578a33
LucieSaulnier <lucie.saulnier1988@gmail.com>
2021-06-15 09:05:03 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Button from './component/Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      capital: '',
+      flag: '',
+      population: '',
+      region: '',
+    }
+  }
+
+  async componentDidMount () {
+    try {
+      const result = await fetch('https://restcountries.eu/rest/v2/name/france')
+      const country = await result.json()
+      this.setState({
+        name: country[0].name,
+        capital: country[0].capital,
+        flag: country[0].flag,
+        population: country[0].population,
+        region: country[0].region,
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  // async getCountry(country) { ...
+  getCountry = async (country) => {
+    try {
+      const result = await fetch('https://restcountries.eu/rest/v2/name/' + country)
+      // const result = await fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+      const countries = await result.json()
+      this.setState({
+        name: countries[0].name,
+        capital: countries[0].capital,
+        flag: countries[0].flag,
+        population: countries[0].population,
+        region: countries[0].region,
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  render () {
+    return <div>
+      <Button onClick={() => this.getCountry('france')}>France</Button>
+      <Button onClick={() => this.getCountry('brazil')}>Brazil</Button>
+      <Button onClick={() => this.getCountry('croatia')}>Croatia</Button>
+      <br/>Name : {this.state.name}
+      <br/>Capital : {this.state.capital}
+      <br/>Flag : {this.state.flag}
+      <br/>Population : {this.state.population}
+      <br/>Region : {this.state.region}
     </div>
-  );
+  }
 }
 
-export default App;
+export default App
+  
